a6e92d1b7b22e05552fd5dc2c66d55f0ede9c9da
SaShimy <antoinegunzburger@hotmail.fr>
2017-07-06 10:59:41 +0000

    test

--- client/client.c
+++ client/client.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
     return 0;
   }
 
-  printf("Connecting..\n");
+  printf("Connecting...\n");
   zsock_t *req = zsock_new(ZMQ_REQ);
   zsock_connect(req, "tcp://localhost:%s", argv[1]);
   printf("Connected\n");
