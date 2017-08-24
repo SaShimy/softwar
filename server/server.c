5cc0d371f08144a291396cffa0b3ea8013b22aff
lindetna <lin_d@etna-alternance.net>
2017-08-24 10:35:39 +0000

    receive send du socket router OK

--- server/server.c
+++ server/server.c
@@ -1,13 +1,8 @@
 #include "server.h"
 
-int server_send_msg(zmsg_t *message, zsock_t *router) {
+int server_send_msg(zframe_t *identity, zframe_t *empty, zframe_t *content, /*void *router) {*/zsock_t *router) {
   zmsg_t *response = zmsg_new();
 
-  // j'ai mis ça car ca bug sinon mais je ne sais plus ce qu'on a fait ici
-  zframe_t *identity = zmsg_pop(message);
-  zframe_t *empty = zmsg_pop(message);
-  zframe_t *content = zmsg_pop(message);
-   
   zmsg_prepend(response, &identity);
   zmsg_append(response, &empty);
   zmsg_append(response, &content);
@@ -19,26 +14,34 @@ int server_send_msg(zmsg_t *message, zsock_t *router) {
   return (0);
 }
 
-int server_rcv_msg(zmsg_t *message) {
-  zframe_t *identity = zmsg_pop(message);
-  zframe_t *empty = zmsg_pop(message);
-  zframe_t *content = zmsg_pop(message);
-
-  zmsg_destroy(&message);
+int server_rcv_msg(zframe_t *content) {
   printf("Content of message is : %s\n", zframe_strdup(content));
+
   return (0);
 }
 
 int listen_rep(t_conf conf) {
+  /*zctx_t *context = zctx_new ();
+    void *router = zsocket_new(context, ZMQ_ROUTER);*/
   zsock_t *router = zsock_new(ZMQ_ROUTER);
+  zmsg_t *message;
+  zframe_t *identity;
+  zframe_t *empty;
+  zframe_t *content;
+  
   zsock_bind(router, "tcp://*:%s", conf.rep_port);
-
   while (!zsys_interrupted) {
-    zmsg_t *message = zmsg_recv(router);
-    server_rcv_msg(message);
-    //server_send_msg(message, router);
+    //while (!zctx_interrupted) {
+    message = zmsg_recv(router);
+    identity = zmsg_pop(message);
+    empty = zmsg_pop(message);
+    content = zmsg_pop(message);
+    zmsg_destroy(&message);
+    server_rcv_msg(content);
+    server_send_msg(identity, empty, content, router);
   }
   zsock_destroy(&router);
+  //zctx_destroy(&context);
   return (0);
 }
 
@@ -74,7 +77,7 @@ int main(int argc, char **argv)
   set_default_conf(&conf);
   get_options(&conf, argc, argv);
   printf("Configuration:\n-Verbose: %d\n-Size: %d\n-Cycle: %d\n-Log file: %s\n-Rep-port: %s\n-Pub-port: %s\n", conf.verbose, conf.size, conf.cycle, conf.log_file_path, conf.rep_port, conf.pub_port);
-  //listen_rep(conf);
-  identify("#0x01");
+  listen_rep(conf);
+  //identify("#0x01");
   return (0);
 }
