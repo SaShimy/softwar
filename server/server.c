d52748101a11d2795c707d6c88d6ee79ba57f86c
SaShimy <antoinegunzburger@hotmail.fr>
2017-09-13 10:53:49 +0200

    init map that works

--- server/server.c
+++ server/server.c
@@ -28,17 +28,21 @@ int get_options(t_conf *conf, int argc, char **argv) {
 
 int main(int argc, char **argv)
 {
+  char **map;
   int i;
+  int j;
+
   t_conf conf;
   set_default_conf(&conf);
   get_options(&conf, argc, argv);
   printf("Configuration:\n-Verbose: %d\n-Size: %d\n-Cycle: %d\n-Log file: %s\n-Rep-port: %s\n-Pub-port: %s\n", conf.verbose, conf.size, conf.cycle, conf.log_file_path, conf.rep_port, conf.pub_port);
-  // char **map = init_map(conf.size);
-  // printf("%s\n", map[0][0]);
+  map = init_map(conf.size);
+  printf("%c\n", map[0][0]);
+  free(map);
   // for (i = 0; i < conf.size; i++) {
   //   printf("%d\n", strlen(map[i]));
   // }
-  listen_rep(conf);
+  // listen_rep(conf);
   //identify("#0x01");
   return (0);
 }
