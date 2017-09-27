09ad5fcd9de6e58d6d1064458475ad5532dbd0cd
SaShimy <antoinegunzburger@hotmail.fr>
2017-09-27 10:57:15 +0200

    no real changes

--- server/server.c
+++ server/server.c
@@ -11,7 +11,7 @@ int get_options(t_conf *conf, int argc, char **argv) {
     {"pub-port", set_pub_port}
   };
 
-  for (i = 1; i < argc; ++i)
+  for (i = 1; i < argc; i++)
   {
     if (argv[i][0] == '-' && argv[i][1] == '-')
     {
@@ -37,12 +37,20 @@ int main(int argc, char **argv)
   get_options(&conf, argc, argv);
   printf("Configuration:\n-Verbose: %d\n-Size: %d\n-Cycle: %d\n-Log file: %s\n-Rep-port: %s\n-Pub-port: %s\n", conf.verbose, conf.size, conf.cycle, conf.log_file_path, conf.rep_port, conf.pub_port);
   map = init_map(conf.size);
-  printf("%c\n", map[0][0]);
+  for (i = 0; i < conf.size; i++)
+  {
+    for (j = 0; j < conf.size; j++)
+    {
+      printf("%c", map[i][j]);
+    }
+    printf("\n");
+  }
+
   free(map);
   // for (i = 0; i < conf.size; i++) {
   //   printf("%d\n", strlen(map[i]));
   // }
-  // listen_rep(conf);
+  listen_rep(conf);
   //identify("#0x01");
   return (0);
 }
